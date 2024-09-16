071c7cb5c6c92defb15adcff921ac85b4142afb1
aditya bhalgat <adityabhalgat81@gmail.com>
2024-09-16 17:13:29 +0530

    Added the speech to text feature

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -3,22 +3,25 @@ import App1 from "./components/App1";
 import App2 from "./components/App2";
 
 export default function App() {
-// In App.js
-const [showApp1, setShowApp1] = useState(true);
+  const [showApp1, setShowApp1] = useState(true);
 
-const handleStartChat = () => {
-  console.log("Button clicked, switching to App2");
-  setShowApp1(false);
-};
+  const handleStartChat = () => {
+    console.log("Button clicked, switching to App2");
+    setShowApp1(false);
+  };
 
-return (
-  <div>
-    {showApp1 ? (
-      <App1 onStartChat={handleStartChat} />
-    ) : (
-      <App2 />
-    )}
-  </div>
-);
+  const handleHomeClick = () => {
+    console.log("Home clicked, switching to App1");
+    setShowApp1(true);
+  };
 
+  return (
+    <div>
+      {showApp1 ? (
+        <App1 onStartChat={handleStartChat} onHomeClick={handleHomeClick} />
+      ) : (
+        <App2 onHomeClick={handleHomeClick} />
+      )}
+    </div>
+  );
 }
